9399c820b9a1c2e80f2f0e91450ac7a0af0136e7
ssb <ss2212693@gmail.com>
2026-06-18 15:24:01 +0530

    initial commit

---
+++ src/main/java/com/tatastrive/campus/centralized_campus_hiring/CentralizedCampusHiringApplication.java
@@ -0,0 +1,13 @@
+package com.tatastrive.campus.centralized_campus_hiring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedCampusHiringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedCampusHiringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tatastrive/campus/centralized_campus_hiring/CentralizedCampusHiringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatastrive.campus.centralized_campus_hiring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizedCampusHiringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
